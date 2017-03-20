5f0f9240465a713766ae111ca157a8b4f7afacb7
rmit-s3579368-Haoran-Zhou <s3579368@student.rmit.edu.au>
2017-03-20 14:56:54 +1100

    test

---
+++ src/test/hello.java
@@ -0,0 +1,20 @@
+package test;
+
+import java.util.Scanner;
+
+public class hello {
+
+
+	public static void main(String args[]){
+		
+		System.out.print("A");
+		System.out.print("B");
+		System.out.println("C");
+		
+		System.out.print("ABC\n");
+	}
+	
+	
+	
+	   }
+
